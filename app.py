2f50acb6e70cbd0419514a611823ce39c8ece1a3
David <Davidchincharashvili@gmail.com>
2021-05-09 13:23:33 +0400

    added readme file

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ class User(db.Model, UserMixin):
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
-    words = db.relationship('Word', backref='users', lazy='dynamic')
+    words = db.relationship('Words', backref='users', lazy='dynamic')
     # https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
 
 
